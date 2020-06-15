0d6438dc7f29d1b385322bfea0befd8669818ffe
Ben Stone <benjamin.h.stone@gmail.com>
2020-06-15 14:19:56 -0400

    Initial Commit

---
+++ ColorGuessingAppWithTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorGuessingAppWithTesting
+//
+//  Created by Benjamin Stone on 6/15/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
